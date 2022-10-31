2dcf76fa559f87e197d89f5e781ba23300712a13
duncan <dmckee95@gmail.com>
2022-10-31 16:04:32 -0400

    Create gameboard method and set basic functions

--- scripts.js
+++ scripts.js
@@ -1,3 +1,29 @@
-let gameBoard = {
-    
-}
+
+(function () {    
+    let gameBoard = {
+
+        init: function(){
+            this.cacheDom();
+            this.bindEvents();
+            console.log("init running")
+        },
+        cacheDom: function(){
+            this.board = document.getElementById("gameArea");
+            this.space = document.querySelectorAll("[id=gameSpot]");
+            this.test = console.log("Dom is caching");            
+        },
+        bindEvents: function(){
+            this.space.forEach(element => {
+            element.addEventListener("click", this.addGamePiece)
+            });
+            this.test = console.log("Events binded")
+        },
+        addGamePiece: function(){
+            console.log("game piece function running")
+        },
+    }
+
+    gameBoard.init();
+    console.log("function self running")
+})();
+
